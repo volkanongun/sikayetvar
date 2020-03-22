0939c1f219ee69c43544d950274eec998e1d8524
Volkan Ongun <me@volkanongun.com>
2020-03-22 17:46:06 +0300

    sass compiler and access log added. images added. Pug sytax working

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+var morgan = require('morgan')
+var fs = require('fs')
+var path = require('path')
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 
 const app = express();
 app.set('view engine', 'pug')
+app.use(express.static('public'))
+app.use(morgan('combined', { stream: accessLogStream }))
 
 app.get('/', (req,res)=>{
 	res.render('index', { title: 'Hey', message: 'Hello there!' })
